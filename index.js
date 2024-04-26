20107491f9e695b747d03421a04406ba21b39bc9
SanimSRk <hasansanim562@gmail.com>
2024-04-26 10:00:09 +0600

    server sit Post data mongodb done

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+
+//TourVibe66
+// 9kUTR2Lj0hKz4xz7
+
+const uri =
+  'mongodb+srv://TourVibe66:9kUTR2Lj0hKz4xz7@cluster0.mqe77mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db('ToursDB');
+    const productCollection = database.collection('Product');
+
+    app.post('/products', async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('This is tarebale server site ');
